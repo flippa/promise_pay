5ca51ecb2b94dc77d5fce63ea53fdf3dd49f7f05
Liam Norton <iamliamnorton@gmail.com>
2014-07-10 15:35:36 +1000

    Remove unused spec

--- lib/promise_pay/marketplace.rb
+++ lib/promise_pay/marketplace.rb
@@ -6,24 +6,19 @@ module PromisePay
     def initialize(options = {})
       @user     = options.fetch :user
       @password = options.fetch :password
-      @request  = build_request
     end
 
     def request_token
-      response = request.execute
+      response = PromisePay::Request.new(
+        path:     path,
+        user:     user,
+        password: password
+      ).execute
 
       JSON.parse(response)["token"]
     end
 
     private
-
-    def build_request
-      PromisePay::Request.new(
-        path:     path,
-        user:     user,
-        password: password
-      )
-    end
 
     def path
       "request_token"
@@ -31,6 +26,5 @@ module PromisePay
 
     attr_reader :user
     attr_reader :password
-    attr_reader :request
   end
 end
--- spec/promise_pay_spec.rb
+++
@@ -1,5 +0,0 @@
-require "spec_helper"
-
-describe PromisePay do
-  it "needs a spec!"
-end
