fbd5b97f7b62f888d9fdbbc23b4e57cdf2ad8634
Liam Norton <iamliamnorton@gmail.com>
2014-07-10 14:19:58 +1000

    Change long, hard to type initialize => init

--- lib/generators/promise_pay/init_generator.rb
+++ lib/generators/promise_pay/init_generator.rb
@@ -2,13 +2,20 @@ require "rails/generators/base"
 
 module PromisePay
   module Generators
-    class InitializeGenerator < Rails::Generators::Base
+    class InitGenerator < Rails::Generators::Base
       source_root File.expand_path("../../templates", __FILE__)
+
+      arguement :email,     type: :string
+      arguement :password,  type: :string
 
       desc "Initalize PromisePay API key"
 
       # run the marketplace token request command
       # write the output to the initializer
+
+      def custom_method_call
+        puts "Yesss i got called..."
+      end
 
       def copy_initializer
         template "promise_pay_initializer.rb", "config/initializers/promise_pay.rb"
