880f574fbe10596d9bd0ae17d3107244bb57485d
Liam Norton <iamliamnorton@gmail.com>
2014-07-10 14:02:10 +1000

    Initalize generator uses a template for initializer

--- lib/generators/promise_pay/initialize_generator.rb
+++ lib/generators/promise_pay/initialize_generator.rb
@@ -3,10 +3,12 @@ require "rails/generators/base"
 module PromisePay
   module Generators
     class InitializeGenerator < Rails::Generators::Base
-      #source_root File.expand_path(File.dirname(__FILE__))
+      source_root File.expand_path("../../templates", __FILE__)
+
+      desc "Initalize PromisePay API key"
 
       def copy_initializer
-        copy_file "promise_pay_initializer.rb", "config/initializers/promise_pay.rb"
+        template "promise_pay_initializer.rb", "config/initializers/promise_pay.rb"
       end
     end
   end
